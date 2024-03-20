94349baf612412659a3e1491bf0c21a755dda5b7
Balliol Chen <140304881+tiyusheng@users.noreply.github.com>
2024-03-20 22:14:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="">
+<head>
+  <meta charset="utf-8">
+  <title>Balliol Chen (陈家洛)</title>
+  <meta name="description" content="I am Balliol Chen. Please feel free to contact me regarding my academic background and my life direction.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta name="keywords" content="applied, math">
+
+  <link href="/static/bc.ico" rel="shortcut icon">
+  <link rel="stylesheet" href="static/css/landing.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="static/css/stylesheet.css" type="text/css" >
+  <link rel="stylesheet" href="static/css/stylesheet1.css" type="text/css" >
+
+  <link rel="preload" as="image" href="static/image/bg.jpeg">
+  <link rel="preload" as="image" href="static/image/noise.svg">
+
+</head>
+<body>
+<div id="bg-container">
+  <div id="bg1"></div>
+  <div id="bg2"></div>
+</div>
+<div id="contentContainer">
+<div id="name">Balliol Chen</div>
+<p><div id="name">陈家洛</div></p>
+<p> </p>
+<p></p>
+
+<div class="menu">
+    <a href="https://research.chen.ba">research</a>
+    <a href="https://research.chen.ba/cv.pdf">vitae</a>
+    <a href="https://t.chenb.com/academic/slides/index.php">slides</a>
+    <a href="https://authorchen.com">diary</a>
+</div>
+
+</div>
+</body>
+</html>
